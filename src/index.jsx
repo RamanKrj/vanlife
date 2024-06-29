32e05b9c6a9c2aed54cb58337d3d54345147a78d
= <rkrjha@outlook.com>
2024-06-29 18:39:20 +0530

    Second commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import './index.css'
+import './server.js'
 
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
@@ -6,19 +7,24 @@ import About from './pages/About.jsx'
 import Home from './pages/Home.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Vans from './pages/Vans.jsx'
+import VansDetail from './pages/VansDetail.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link to="/">#VANLIFE</Link>
+        <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VansDetail />} />
       </Routes>
     </BrowserRouter>    
   )
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom"
+import React from "react"
+import VansDetail from "./VansDetail.jsx"
+
+export default function Vans() {
+  const [vans, setVans] = React.useState([])
+  
+  React.useEffect(() => {
+    fetch('/api/vans')
+      .then(res => res.json())
+      .then(data => setVans(data.vans))
+  }, [])
+  
+  const vanAll = vans.map(van => (
+    <div key={van.id} className="van-tile">
+      <Link to={`/vans/${van.id}`} element={<VansDetail />}>
+        <img 
+          src={van.imageUrl} 
+          alt={`Image of the ${van.name}`}
+          aria-label={`View details for ${van.name} priced at $${van.prce} per day`}
+        />
+        <div className="van-info">
+          <p>{van.name}</p>
+          <p>${van.price}<span>/day</span></p>
+        </div>
+        <i className={`van.type ${van.type} selected`}>{van.type}</i>
+      </Link>  
+    </div>
+  ))
+  return (
+    <div className="van-list-container">
+      <h1>Explore your vans</h1>
+      <div className="van-list">
+        {vanAll}
+      </div>
+    </div>
+  )
+}
